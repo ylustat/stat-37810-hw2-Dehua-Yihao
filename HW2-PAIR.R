0ad17c8208efb45b7973ff56dd8fcfc041527175
ylustat <yihaolu@uchicago.edu>
2020-10-11 01:13:13 -0500

    Added solutions to incorrect input.

--- HW2-PAIR.R
+++ HW2-PAIR.R
@@ -1,19 +1,32 @@
 generate_computer_vector <- function(min = 1, max = 10) {
   if(max - min + 1 <= 4) {
-    stop("The range should at least include 5 numbers to start the game!")
+    cat("The range should at least include 5 numbers to start the game!")
   }
   computer_vector <- sample(min:max, 4, replace = F)
   return(computer_vector)
 }
-get_guess <- function(min = 1, max = 10, terminal=F) {
+get_guess <- function(min = 1, max = 10, terminal=F, incorrect) {
   if(terminal) {
     cat(paste0("Please enter four numbers ranging from ",min," to ",max,": > "))
     numbers_string <- readLines("stdin", 1)
   } else {
     numbers_string <- readline(paste0("Please enter four numbers ranging from ",min," to ",max,": > "))
   }
-  user_choice <- as.integer(unlist(strsplit(numbers_string, " ")))
-  return(user_choice)
+  user_choice <- suppressWarnings(as.integer(unlist(strsplit(numbers_string, " "))))
+  if(length(user_choice)!=4) {
+    incorrect <- incorrect + 1
+    cat(paste0("You have to input 4 numbers! This is your ",incorrect," time of incorrect input. After three times of incorrect input the game will be over!\n"))
+  } else if (sum(user_choice<0,na.rm = T)>0){
+    incorrect <- incorrect + 1
+    cat(paste0("You have to input 4 positive numbers! This is your ",incorrect," time of incorrect input. After three times of incorrect input the game will be over!\n"))
+  } else if(sum(is.na(user_choice))>0) {
+    incorrect <- incorrect + 1
+    cat(paste0("You have to input 4 numbers and not include any characters! This is your ",incorrect," time of incorrect input. After three times of incorrect input the game will be over!\n"))
+  }else if(sum(duplicated(user_choice,na.rm = T))>0) {
+    incorrect <- incorrect + 1
+    cat(paste0("You have to input 4 different numbers! This is your ",incorrect," time of incorrect input. After three times of incorrect input the game will be over!\n"))
+  } 
+  return(list(user_choice,incorrect))
 }
 number_bulls_and_cows <- function(user_choice, computer_vector) {
   bulls <- rep(FALSE,4)
@@ -55,9 +68,21 @@ terminal = as.numeric(args[1])
 if(is.na(terminal)) terminal <- FALSE
 
 computer_vector <- generate_computer_vector()
+incorrect <- 0
 for(i in 1:10){
-  user_choice <- get_guess(terminal=terminal)
-  number <- number_bulls_and_cows(user_choice,computer_vector)
-  do_response(i, number[[1]],number[[2]])
+  incorrect_init <- incorrect
+  user_choice <- get_guess(terminal=terminal,incorrect = incorrect)
+  incorrect <- user_choice[[2]]
+  user_choice <- user_choice[[1]]
+  if(incorrect - incorrect_init == 0) {
+    number <- number_bulls_and_cows(user_choice,computer_vector)
+    do_response(i, number[[1]],number[[2]])
+  } 
+  if(incorrect == 3) {
+    print("You have lost, the number computer guessed is ")
+    print(computer_vector)
+    break
+  }
+
 }
 
--- HW2.md
+++ HW2.md
@@ -36,3 +36,21 @@ Please enter four numbers ranging from 1 to 10: > 5 8 7 3
 [1] 5 7 8 3
 ```
 
+##Question 3
+
+Output of the results: the function can identify the problems in input and after three times of incorrect input it will end and output the correct answer.
+
+```R
+> source('~/Desktop/UChicago/2020-2021/2020Fall/Computing/stat-37810-hw2-Dehua-Yihao/HW2-PAIR.R')
+Please enter four numbers ranging from 1 to 10: > 1 2 3 4
+[1] "You have guessed  0  bulls, and  1  cows, you have  9  times left."
+Please enter four numbers ranging from 1 to 10: > a b c d
+You have to input 4 numbers and not include any characters! This is your 1 time of incorrect input. After three times of incorrect input the game will be over!
+Please enter four numbers ranging from 1 to 10: > -1 2 3 4
+You have to input 4 positive numbers! This is your 2 time of incorrect input. After three times of incorrect input the game will be over!
+Please enter four numbers ranging from 1 to 10: > 1 2 3 4 5
+You have to input 4 numbers! This is your 3 time of incorrect input. After three times of incorrect input the game will be over!
+[1] "You have lost, the number computer guessed is "
+[1] 10  5  6  3
+```
+
